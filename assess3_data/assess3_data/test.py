5f48cf8f779f2a549fe8b95c8de4121158675a40
Anh Duc Vu <ducanh.vu0809@gmail.com>
2024-09-03 01:48:57 +0700

    new/task1
    commit

--- assess3_data/assess3_data/test.py
+++
@@ -1,4 +0,0 @@
-from collections import Counter
-
-sample_list = ['senior', 'negotiator', 'location', 'shoreditch', 'depending', 'ote', 'car', 'allowance', 'bridge', 'shoreditch', 'type', 'permanent', 'brilliant', 'proven']
-print(Counter(sample_list))
